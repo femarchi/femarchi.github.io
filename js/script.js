8cd6aefa456dd4923ec55dc7e5a7a41e5cbdbdd4
femarchi <fepmarchi@gmail.com>
2017-02-02 18:18:06 -0200

    Removed commented HTML

--- js/script.js
+++ js/script.js
@@ -77,34 +77,34 @@ $(document).ready(function() {
 
 // Smooth Scrolling
 
-	var navHeight = document.getElementById('nav-bar').offsetHeight;
+	// var navHeight = document.getElementById('nav-bar').offsetHeight;
 
-	$(".portfolio-web-btn").click(function() {
-	    $("html, body").animate({
-	        scrollTop: $("#portfolio-web").offset().top-navHeight},
-	        'slow');
-	});
+	// $(".portfolio-web-btn").click(function() {
+	//     $("html, body").animate({
+	//         scrollTop: $("#portfolio-web").offset().top-navHeight},
+	//         'slow');
+	// });
 
-	$(".portfolio-3d-btn").click(function() {
-	    $("html, body").animate({
-	        scrollTop: $("#portfolio-3d").offset().top-navHeight},
-	        'slow');
-	});
+	// $(".portfolio-3d-btn").click(function() {
+	//     $("html, body").animate({
+	//         scrollTop: $("#portfolio-3d").offset().top-navHeight},
+	//         'slow');
+	// });
 
-	$(".contact-btn").click(function() {
-	    $("html, body").animate({
-	        scrollTop: $("#contact").offset().top-navHeight},
-	        'slow');
-	});
+	// $(".contact-btn").click(function() {
+	//     $("html, body").animate({
+	//         scrollTop: $("#contact").offset().top-navHeight},
+	//         'slow');
+	// });
 
-	$("#form-submit-btn").click(function() {
-		$('#form').attr('action',
-                       'mailto:fepmarchi@gmail.com?subject=' +
-                       $('#form-subject').val() + '&body=' +
-                       $('#form-message').val() + '\n' + $('#form-name'));
-        $('#form').submit();
-		alert("Obrigado!");
-	});
+	// $("#form-submit-btn").click(function() {
+	// 	$('#form').attr('action',
+ //                       'mailto:fepmarchi@gmail.com?subject=' +
+ //                       $('#form-subject').val() + '&body=' +
+ //                       $('#form-message').val() + '\n' + $('#form-name'));
+ //        $('#form').submit();
+	// 	alert("Obrigado!");
+	// });
 
 
 }); //end of $(document).ready();
