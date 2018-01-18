9cb9c203804dfd7f5e25a18c65b866cc397b8d4e
femarchi <fepmarchi@gmail.com>
2018-01-18 10:48:43 -0200

    minor fixes and changes

--- js/script.js
+++ js/script.js
@@ -21,6 +21,7 @@ $(document).ready(function() {
     return arr;
 	}
 
+	//shuffle projects on page open
 	function shuffle(array){
 
 		var arrayClone = JSON.parse(JSON.stringify(array));
@@ -56,9 +57,9 @@ $(document).ready(function() {
 			//populate template
 			$(clonedTemplate).find('.project-img').attr('id', array[i].id);
 			$(clonedTemplate).find('img').attr('src', array[i].image);
-			$(clonedTemplate).find('a').attr('href', array[i].link);
+			// $(clonedTemplate).find('a').attr('href', array[i].link);
 			clonedTemplate.querySelector('h5').textContent = array[i].title;
-			clonedTemplate.querySelector('p').textContent = array[i].description;
+			clonedTemplate.querySelector('p').textContent = array[i].brief;
 			if(array[i].centerfocus){
 				$(clonedTemplate).find('img').addClass('center-focus');
 			}
@@ -77,7 +78,7 @@ $(document).ready(function() {
 		var clonedTemplate = content.cloneNode(true);
 
 		//console.log(project);
-		console.log(project.id);
+		// console.log(project.id);
 		var title, image, description;
 
 		//find project by id
@@ -96,9 +97,16 @@ $(document).ready(function() {
 		//reset html
 		$('#portfolio').html("");
 
-		clonedTemplate.querySelector('h5').textContent = title;
+		//insert blown description
+		clonedTemplate.querySelector('h3').textContent = title;
 		$(clonedTemplate).find('img').attr('src', image);
 		clonedTemplate.querySelector('p').textContent = description;
+		if(link != "#"){
+			$(clonedTemplate).find('a').attr('href', link);
+			clonedTemplate.querySelector('a').textContent = "Clique aqui";
+			$(clonedTemplate).find('.project-page-description-text').textContent = "Para ver um protótipo do site ";
+		}
+		
 
 		//insert clone into portfolio
 		document.getElementById('portfolio').appendChild(clonedTemplate);
